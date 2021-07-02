06ebfa55f6b00ab99831c3eca3f90c87009e91f5
Safeer Chonengal <safeer2c@gmail.com>
2021-07-02 06:37:11 +0530

    Added comment

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const { app, PORT } = require("./app");
 require("./apiTest");
 require("./chatWS");
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`App listening at http://localhost:${PORT}`);
 });
